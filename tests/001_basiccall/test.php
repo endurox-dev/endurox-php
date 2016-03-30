bd49b4ffdee12a49a1637191a62f88da2067153f
ATR Baltic, SIA <madars.vitolins@gmail.com>
2016-03-31 02:03:26 +0300

    Update test.php

--- tests/001_basiccall/test.php
+++ tests/001_basiccall/test.php
@@ -14,8 +14,6 @@ if (0>ndrx_tpcall ("UNIXINFO", $buf, $buf, 0))
         print("call failed UNIXINFO: ".ndrx_tpstrerror(ndrx_get_tperrno())."\n");
 }
 
-$myfile = fopen("bufer.txt", "w");
-
 /* for output see: /tmp/php_ndrx.out */
 ndrx_bfprintf($buf);
 
